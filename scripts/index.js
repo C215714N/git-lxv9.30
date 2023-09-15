e7a8b09e25de9f7d59ded57b8c7ef480da0809a3
El parce XD <andresclavijo0822@gmail.com>
2023-09-15 11:15:01 -0300

    [UPDATE]

--- scripts/index.js
+++ scripts/index.js
@@ -10,4 +10,14 @@ const clickAndres = document.querySelector(".click");
 clickAndres.addEventListener("click", () => {
     alert("HOLA A TODOS");
     alert("VAMOS A PROGRAMAR CON CRISTIAN");
+
+    for (let i = 1; i < 5; i++) {
+        while (i <= 5) {
+            alert(i);
+            i++
+        }
+        if (5 === 5) {
+            window.location.href = "https://i.blogs.es/0ec27f/memess/1024_2000.webp";
+        }
+    }
 });
