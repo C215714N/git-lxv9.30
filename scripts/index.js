e0e84f7902dd32864e6a4251cc31f7ba0f340bfc
El parce XD <andresclavijo0822@gmail.com>
2023-09-15 10:37:07 -0300

    Hice colaboradores y un script

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,13 @@
 /* Declaraciones */
 
+const clickAndres = document.querySelector(".click");
 
 /* Funciones */
 
 
 /* Eventos */
 
-
+clickAndres.addEventListener("click", () => {
+    alert("HOLA A TODOS");
+    alert("VAMOS A PROGRAMAR CON CRISTIAN");
+});
